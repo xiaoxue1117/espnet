fa220cda4f8a115a0a62f686e78a3e21af208049
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2022-03-02 15:42:37 -0500

    l

--- espnet2/asr/espnet_model.py
+++ espnet2/asr/espnet_model.py
@@ -324,8 +324,11 @@ class ESPnetASRModel(AbsESPnetModel):
                 # Pre-encoder, e.g. used for raw input data but here used as a linear layer only ! 
                 feats_hubert = self.project_hubert(feats_hubert)
                 a,b,c = feats_hubert.shape
+                assert 6==0, a
                 self.feats_hubert=feats_hubert.view(13,-1,b,c)
                 self.feats_lengths_hubert=feats_lengths_hubert.view(13,-1,b,c)
+
+                self.feats_hubert=self.feats_hubert[x for x in self.layer_selection_hubert]
                 assert 6==0, self.feats_hubert.shape
                 
                 # mettre la gate ICI sur MFCC only :
