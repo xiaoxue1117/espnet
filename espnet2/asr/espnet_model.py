c60b7570ca70aa1efc109f07714da053feed3645
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2022-03-02 21:39:11 -0500

    l

--- espnet2/asr/espnet_model.py
+++ espnet2/asr/espnet_model.py
@@ -385,8 +385,8 @@ class ESPnetASRModel(AbsESPnetModel):
             m = min(feats_hubert.shape[1],encoder_out.shape[1])
             diff = max(feats_hubert.shape[1]-m, encoder_out.shape[1]-m)
             assert diff<8, "we had to drop {} frames, this seems to be too much".format(diff)
-            encoder_out, feats_hubert, MOE_weights = encoder_out[:,:m,:], self.feats_hubert[:,:m,:], MOE_weights[:,m,:]
-            assert 7==0, (MOE_weights.shape, m)
+            encoder_out, feats_hubert, MOE_weights = encoder_out[:,:m,:], self.feats_hubert[:,:m,:], MOE_weights[:,:m,:]
+           # assert 7==0, (MOE_weights.shape, m)
             w_fbank = MOE_weights[:,:,:,0]
             w_hub = [MOE_weights[:,:,:,1+i] for i in range(len(self.layer_selection_hubert))]
 
